1024ab29e89966f70b7632052dac39e0105f9310
Jos Vermoesen <info@rv.be>
2023-02-18 10:10:44 +0100

    pointer

---
+++ GCC/99 array vs pointer/string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    char s1[] = "Hello vscode";
+    s1[0] = 'h';
+    printf("s1: %s\n", s1);
+
+    char s2[] = {'H', 'e', 'l', 'l', 'o', ' ', 'v', 's', 'c', 'o', 'd', 'e', '\0'};
+    s2[1] = 'a';
+    printf("s2: %s\n", s2);
+
+    char *s3 = "Hello vscode"; // string literal somewhere in memory sizeof(s3) = 13
+    // s3[0] = 'h'; // error: assignment of read-only location ‘*s3’
+    printf("s3: %s\n", s3);
+
+    s3 = "new string"; // another string literal somewhere in memory sizeof(s3) = 8
+    printf("s3: %s\n", s3);
+    printf("sizeof(s1): %d\n", sizeof(s1)); // sizeof(s1) = 13
+    printf("sizeof(s2): %d\n", sizeof(s2)); // sizeof(s2) = 13 
+    printf("sizeof(s3): %d\n", sizeof(s3)); // sizeof(s3) = 8
+}
+/* Source:
+   https://www.youtube.com/watch?v=Qp3WatLL_Hc
+*/
