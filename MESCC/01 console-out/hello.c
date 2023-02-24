04e04532d01ffb0470dee27659b1240cf617dd01
Jos Vermoesen <info@rv.be>
2023-02-24 14:01:09 +0100

    digitcount

--- MESCC/01 console-out/hello.c
+++ MESCC/01 console-out/hello.c
@@ -31,6 +31,20 @@
 	05 Apr 2016 : v1.01 Minor changes.
 */
 
+/* Defines for MESCC libraries
+   ---------------------------
+   Leave only what you need.
+*/
+// #define CC_STDIO        // Support for stdin, stdout, stderr.
+// #define CC_REDIR        // Support for command line redirection - needs CC_STDIO.
+// #define CC_CONIO_BIOS   // Console I/O through BIOS instead of BDOS.
+// #define CC_FCX          // Support for user number in filenames.
+// #define CC_FCX_DIR      // Support for named user numbers - needs CC_FCX and DirToDrvUsr().
+// #define CC_FOPEN_A	// Enable modes "a" and "ab" for fopen.
+// #define CC_FREAD	// Include fread().
+// #define CC_FWRITE	// Include fwrite().
+// #define CC_FGETS	// Include fgets().
+
 /* Some defs. to save memory
  */
 #define CC_NO_MUL	 // To exclude MULTIPLICATION code.
@@ -38,10 +52,35 @@
 #define CC_NO_SWITCH // To exclude SWITCH code.
 #define CC_NO_ARGS	 // To exclude ARGC & ARGV code.
 
-/* Standard libraries
- */
-#include "mescc.h"
-#include "conio.h"
+/* Standard MESCC library
+   ----------------------
+   You must include this library;
+   it contains some defs. and the runtime.
+*/
+#include <mescc.h>
+
+/* Standard MESCC libraries
+   ------------------------
+   Use only what you need.
+*/
+// #include <alloc.h>
+// #include <atexit.h>
+// #include <bsearch.h>
+// #include <clock.h>
+#include <conio.h>
+// #include <cpm.h>
+// #include <ctype.h>
+// #include <fileio.h>
+// #include <fprintf.h>
+// #include <mem.h>
+// #include <printf.h>
+// #include <qsort.h>
+// #include <rand.h>
+// #include <setjmp.h>
+// #include <sprintf.h>
+// #include <stdbool.h>
+// #include <string.h>
+// #include <z80.h>
 
 /* The real code
  */
