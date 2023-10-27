05fb1293a2880d93a8e96c05b5a3e7f6ba2b1296
JosVermoesen <josvermoesen@outlook.be>
2023-10-27 12:13:33 +0200

    cont

--- PC6502/ascii-fun.c
+++ PC6502/ascii-fun.c
@@ -2,9 +2,11 @@
     Experimenting ASCII values and escape codes in C.
  */
 
+#include <rp6502.h>
 #include <stdio.h>
 
 static const char *CSI = "\33[";  // escape
+
 static const char *BLACK = "30m"; // color codes
 static const char *RED = "31m";
 static const char *GREEN = "32m";
@@ -13,19 +15,50 @@ static const char *MAGENTA = "35m";
 static const char *CYAN = "36m";
 static const char *WHITE = "37m";
 static const char *YELLOW = "93m";
-static const char *UNDEFINED = "30m";
+static const char *UNDEFINED = "37m";
+
+const int black = 0;
+const int red = 1;
+const int green = 2;
+const int yellow = 3;
+const int blue = 4;
+const int magenta = 5;
+const int cyan = 6;
+const int white = 7;
 
 void clear()
 {
     puts("\30\f");
+    // printf("\f"); // also clear console
 }
 
-void colorSet(int colorCode)
+void setColor(colorCode)
 {
     switch(colorCode)
     {
+        case 0:
+            printf("%s%s", CSI, BLACK);
+            break;
         case 1:
+            printf("%s%s", CSI, RED);
+            break;
+        case 2:
+            printf("%s%s", CSI, GREEN);
+            break;
+        case 3:
+            printf("%s%s", CSI, YELLOW);
+            break;
+        case 4:
             printf("%s%s", CSI, BLUE);
+            break;
+        case 5:
+            printf("%s%s", CSI, MAGENTA);
+            break;
+        case 6:
+            printf("%s%s", CSI, CYAN);
+            break;
+        case 7:
+            printf("%s%s", CSI, WHITE);
             break;
 
         // operator doesn't match any case constant +, -, *, /
@@ -39,15 +72,8 @@ void colorSet(int colorCode)
 void charset()
 {
     int i;
-    clear();
-    colorSet(1);
-
-    // static const char * CSI = "\33[";
-    // printf("%s%s", CSI, "31m"); // RED
-
-    // printf("\f"); // clear console
-    // puts("\30\f");
-    // printf("\e[1;1H\e[2J");
+    setColor(green);
+    clear();   
 
     /* Print ASCII values from 0 to 255 */
     for (i = 32; i <= 221; i++)
@@ -56,7 +82,9 @@ void charset()
         printf(" ");
     }
 
-    printf("\n");
+    printf("Press any key to continue.\n");
+    setColor(white);
+    clear();
 }
 
 int main()
