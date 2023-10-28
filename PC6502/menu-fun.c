5fc4842f378ebefc087378a3cbe1b2235445c47a
Jos Vermoesen <info@rv.be>
2023-10-28 07:53:17 +0200

    menu-fun

---
+++ PC6502/menu-fun.c
@@ -0,0 +1,190 @@
+/*
+    Menu example in C.
+    https://www.scaler.com/topics/menu-driven-program-in-c/
+ */
+// #define SO_6502
+#define SO_DOSWIN
+
+#ifdef SO_6502
+    #include <rp6502.h>
+    #include <stdio.h>
+#else
+    #include <conio.h>
+    #include <stdio.h>    
+#endif
+
+static const char *CSI = "\33["; // escape
+
+const int black = 0;
+const int red = 1;
+const int green = 2;
+const int yellow = 3;
+const int blue = 4;
+const int magenta = 5;
+const int cyan = 6;
+const int white = 7;
+
+void clear()
+{
+    #ifdef SO_6502
+        puts("\30\f"); // rp6502 and others
+        // printf("\f"); // also clears console
+    #else
+        printf("\e[1;1H\e[2J"); // DOS/WINDOWS
+    #endif    
+}
+
+void setColor(int colorCode)
+{
+    /*
+        static const char *BLACK = "30m"; // color codes
+        static const char *RED = "31m";
+        static const char *YELLOW = "93m";
+        static const char *GREEN = "32m";
+        static const char *BLUE = "34m";
+        static const char *MAGENTA = "35m";
+        static const char *CYAN = "36m";
+        static const char *WHITE = "37m";
+        static const char *UNDEFINED = "37m";
+     */
+    switch (colorCode)
+    {
+    case 0: // BLACK
+        printf("%s%s", CSI, "30m");
+        break;
+    case 1: // RED
+        printf("%s%s", CSI, "31m");
+        break;
+    case 2: // GREEN
+        printf("%s%s", CSI, "32m");
+        break;
+    case 3: // YELLOW
+        printf("%s%s", CSI, "93m");
+        break;
+    case 4: // BLUE
+        printf("%s%s", CSI, "34m");
+        break;
+    case 5: // MAGENTA
+        printf("%s%s", CSI, "35m");
+        break;
+    case 6: // CYAN
+        printf("%s%s", CSI, "36m");
+        break;
+    case 7: // WHITE
+        printf("%s%s", CSI, "37m");
+        break;
+
+    // operator doesn't match any case constant +, -, *, /
+    default:
+        printf("Error! operator is not correct. Switching to white");
+        printf("%s%s", CSI, "37m");
+    }
+}
+
+void anyKey()
+{    
+    char c;    
+    #ifdef SO_6502
+        while (!(RIA.ready & RIA_READY_RX_BIT))
+            ;
+        c = RIA.rx;
+    #else
+        c = getch();
+    #endif    
+    // printf("pressed: %c %d\n", c, c);
+
+    /* Use != EOF eventually, a value that is returned
+       by getchar() when the end of the file is reached.
+       Use CTRL+C to send EOF to the program. Does not work
+       always in the console window.
+    */
+    /* long nc;
+
+    nc = 0;
+    printf("Press some keys please (Q to quit) ");
+    while (getchar() != 'Q')
+
+        ++nc;
+    printf("You pressed %ld times\n", nc); */
+}
+
+void charset(int aFrom, int aTo)
+{
+    int i;
+
+    /* Print ASCII values from 0 to 255 */
+    for (i = aFrom; i <= aTo; i++)
+    {
+        printf("%c: %d", i, i);
+        printf(" ");
+    }
+}
+
+int main()
+{   
+    clear();
+    setColor(green);
+
+    // Choice variable
+    int choice = -1;
+    
+    // Variables to store the numbers
+    int a, b;
+    
+    // Menu display
+    printf("MENU:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Exit\n");
+    
+    // Infinite Loop for choice input
+    while(1){
+        printf("\nEnter the operation you wish to perform:");
+        scanf("%d", &choice);
+        
+        // If-else ladder
+        if(choice == 1){
+            // Number Input
+            printf("Enter First number :");
+            scanf("%d", &a);
+            printf("Enter Second number:");
+            scanf("%d", &b);
+            printf("\nResult: %d + %d = %d\n", a, b, (a+b));
+            anyKey();
+        }
+        else if (choice == 2){
+            // Number Input
+            printf("Enter First number :");
+            scanf("%d", &a);
+            printf("Enter Second number:");
+            scanf("%d", &b);
+            printf("\nResult: %d - %d = %d\n", a, b, (a-b));
+            anyKey();
+        }
+        else if (choice == 3){
+            // Number Input
+            printf("Enter First number :");
+            scanf("%d", &a);
+            printf("Enter Second number:");
+            scanf("%d", &b);
+            printf("\nResult: %d * %d = %d\n", a, b, (a*b));
+            anyKey();
+        }
+        else if (choice == 4){
+            // Number Input
+            printf("Enter First number :");
+            scanf("%d", &a);
+            printf("Enter Second number:");
+            scanf("%d", &b);
+            printf("\nResult: %d / %d = %d\n", a, b, (a/b));
+            anyKey();
+        }
+        else if (choice == 5){
+            printf("BYE!!!\n");
+            // Termination of the Loop using break statement
+            break;
+        }
+        else{
+            printf("> Invalid Input\n");
+        }
+    }
+    
+    return 0;
+}
